f4966384faefd6c74aa202bd03a3997cbc662df3
Victorpraise5 <victorpraise949@gmail.com>
2024-04-08 02:22:10 -0700

    4squaresss

--- study_with_Python/python-classes_and_objects/4-square.py
+++ study_with_Python/python-classes_and_objects/4-square.py
@@ -6,12 +6,24 @@ class Square:
 
     def __init__(self, size=0):
         """initializing data"""
-        if isinstance(size, int):
-            if size >= 0:
-                self.__size = size
+        self.size = size
+
+    @property
+    def size(self):
+        """Retunrs the size of the square instance """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of a square instance"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
             else:
-                raise ValueError("size must be >=o")
+                raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
 
-    def
+    def area(self):
+        """Calculates the Square's area"""
+        return self.__size ** 2
